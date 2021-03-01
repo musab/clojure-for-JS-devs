a0a839acdbddfcaab9a55029cf4254e64bbdd21e
Musa Barighzaai <musa@circleci.com>
2021-03-01 10:17:19 -0500

    config update

--- theme.config.js
+++ theme.config.js
@@ -1,28 +1,28 @@
 export default {
-  repository: "https://github.com/shuding/nextra", // project repo
-  docsRepository: "https://github.com/shuding/nextra", // docs repo
-  branch: "master", // branch of docs
+  repository: "https://github.com/musab/clojure-for-JS-devs", // project repo
+  docsRepository: "https://github.com/musab/clojure-for-JS-devs", // docs repo
+  branch: "main", // branch of docs
   path: "/", // path of docs
-  titleSuffix: " – Nextra",
+  titleSuffix: " – clj for JS devs",
   nextLinks: true,
   prevLinks: true,
   search: true,
   customSearch: null, // customizable, you can use algolia for example
   darkMode: true,
   footer: true,
-  footerText: "MIT 2020 © Shu Ding.",
+  footerText: "MIT 2020 © Musa Barighzaai.",
   footerEditOnGitHubLink: true, // will link to the docs repo
   logo: (
     <>
       <svg>...</svg>
-      <span>Next.js Static Site Generator</span>
+      <span>TODO</span>
     </>
   ),
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Nextra: the next docs builder" />
-      <meta name="og:title" content="Nextra: the next docs builder" />
+      <meta name="description" content="Clojure for JS Devs" />
+      <meta name="og:title" content="Clojure for JS Devs" />
     </>
   ),
 };
